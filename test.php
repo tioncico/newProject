fe3930a6cc09a8f34bb42f8ef1429b894d3eb2a2
tioncico <1067197739@qq.com>
2021-01-12 10:24:38 +0800

    fix

--- test.php
+++ test.php
@@ -13,24 +13,51 @@ go(function () {
     //生成基础类
     $generation = new \EasySwoole\CodeGeneration\InitBaseClass\UnitTest\UnitTestGeneration();
     $mysqlConfig = new \EasySwoole\ORM\Db\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL'));
-
     //获取连接
     $connection = new \EasySwoole\ORM\Db\Connection($mysqlConfig);
     $tableName = 'admin_user_list';
+    //获取数据表结构对象
+    $tableObjectGeneration = new \EasySwoole\ORM\Utility\TableObjectGeneration($connection, $tableName);
+    $schemaInfo = $tableObjectGeneration->generationTable();
 
-    $codeGeneration = new EasySwoole\CodeGeneration\CodeGeneration($tableName, $connection);
-    //生成model
-    $codeGeneration->generationModel("\\Admin", '', \App\Model\BaseModel::class);
+    $tablePre = '';//表前缀
+    $path = "App\\Model";
+    $extendClass = \App\Model\BaseModel::class;
+    $modelConfig = new \EasySwoole\CodeGeneration\ModelGeneration\ModelConfig($schemaInfo, $tablePre, "{$path}", $extendClass);
+    $modelGeneration = new \EasySwoole\CodeGeneration\ModelGeneration\ModelGeneration($modelConfig);
+    $modelName = $modelGeneration->getPhpNamespace()->getName().'\\'.$modelGeneration->getClassName();
+    $result = $modelGeneration->generate();
+    var_dump($result);//生成成功返回生成文件路径,否则返回false
 
-    //生成controller
-    $codeGeneration->generationController("\\Api\\User", null, '', \App\HttpController\Api\User\UserBase::class);
-    //生成unitTest
-    $codeGeneration->generationUnitTest("\\User", null,null,'',\UnitTest\User\UserBaseTestCase::class);
+    $path = "App\\HttpController\\User";
+    $extendClass = \App\HttpController\Api\User\UserBase::class;
+    $modelClass = $modelName;
+    $controllerConfig = new \EasySwoole\CodeGeneration\ControllerGeneration\ControllerConfig($modelClass, $schemaInfo, $tablePre, "{$path}", $extendClass);
+    $controllerGeneration = new \EasySwoole\CodeGeneration\ControllerGeneration\ControllerGeneration($controllerConfig);
+    $result = $controllerGeneration->generate();
+    var_dump($result);
 
-    //生成controller
-    $codeGeneration->generationController("\\Api\\Admin", null, '', \App\HttpController\Api\Admin\AdminBase::class);
-    //生成unitTest
-    $codeGeneration->generationUnitTest("\\Admin", null,null,'',\UnitTest\Admin\AdminBaseTestCase::class);
+    $path = "App\\HttpController\\Admin";
+    $extendClass = \App\HttpController\Api\Admin\AdminBase::class;
+    $modelClass = $modelName;
+    $controllerConfig = new \EasySwoole\CodeGeneration\ControllerGeneration\ControllerConfig($modelClass, $schemaInfo, $tablePre, "{$path}", $extendClass);
+    $controllerGeneration = new \EasySwoole\CodeGeneration\ControllerGeneration\ControllerGeneration($controllerConfig);
+    $result = $controllerGeneration->generate();
+    var_dump($result);
+
+
+    $path = "UnitTest\\Admin";
+    $modelClass = \App\Model\UserModel::class;
+    $controllerClass= \App\HttpController\User::class;
+    $extendClass = \PHPUnit\Framework\TestCase::class;
+    $tablePre = '';//表前缀
+    $controllerConfig = new \EasySwoole\CodeGeneration\UnitTest\UnitTestConfig($modelClass, $controllerClass, $schemaInfo, $tablePre, "{$path}", $extendClass);
+    $controllerConfig->setRootPath(EASYSWOOLE_ROOT);
+    $unitTestGeneration = new \EasySwoole\CodeGeneration\UnitTest\UnitTestGeneration($controllerConfig);
+    $result = $unitTestGeneration->generate();
+    var_dump($result);
+
+
 
     \EasySwoole\Component\Timer::getInstance()->clearAll();
 });
